70ff44674ef6ac54c67879c87f0c27cbb1f9475b
OnlyNoodles <balakrishn4@middleton.school.nz>
2023-03-14 10:13:00 +1300

    ye

--- Movie_Tickets_v1.py
+++ Movie_Tickets_v1.py
@@ -1,1 +1,20 @@
-print("Hello World")
+"""Movie theatre ticketing system - v1
+Welcome screen and variables
+Created by Kavin Balakrishnan
+"""
+
+
+# Component 1 - welcome screen and set up variables
+def sell_ticket():
+    print("********** Fanfare Movies - Ticketing System **********\n")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets= 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sales = 0
+
+
+# main routine
+sell_ticket()
